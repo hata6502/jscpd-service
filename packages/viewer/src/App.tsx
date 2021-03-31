821a381365a33d61e0aa6549df011911b98affd2
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2021-04-01 01:32:44 +0900

    Implement report fetching

--- packages/viewer/src/App.tsx
+++ packages/viewer/src/App.tsx
@@ -7,6 +7,7 @@ import {
 import { Animation, EventTracker, Palette } from "@devexpress/dx-react-chart";
 import AppBar from "@material-ui/core/AppBar";
 import Box from "@material-ui/core/Box";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
@@ -20,15 +21,51 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { useTheme } from "@material-ui/core/styles";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import Alert from "@material-ui/lab/Alert";
+import { useEffect, useState } from "react";
 import type { FunctionComponent } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { vs2015 } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const App: FunctionComponent = () => {
-  const data = [
-    { type: "Unduplicated lines", count: 90 },
-    { type: "Duplicated lines", count: 10 },
-  ];
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [jscpdReport, setJSCPDReport] = useState();
+
+  useEffect(() => {
+    let isMounted = true;
+
+    (async () => {
+      try {
+        const response = await fetch(
+          // TODO: remove test file
+          "jscpd-report.json"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const jscpdReport = await response.json();
+
+        if (isMounted) {
+          setJSCPDReport(jscpdReport);
+        }
+      } catch {
+        if (isMounted) {
+          setErrorMessage("Couldn't load the jscpd report.");
+        }
+      } finally {
+        if (isMounted) {
+          setIsLoading(false);
+        }
+      }
+    })();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   const theme = useTheme();
 
@@ -52,95 +89,104 @@ const App: FunctionComponent = () => {
 
       <main>
         <Container maxWidth="md">
-          <Box mb={4}>
-            <Grid container spacing={4} alignItems="baseline">
-              <Grid item>
-                <Typography variant="h4">
-                  <GitHubIcon fontSize="inherit" />
-                  &nbsp;user/repository
+          {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+
+          {isLoading && (
+            <Box textAlign="center">
+              <CircularProgress />
+            </Box>
+          )}
+
+          {jscpdReport && (
+            <>
+              <Box mb={4}>
+                <Grid container spacing={4} alignItems="baseline">
+                  <Grid item>
+                    <Typography variant="h4">
+                      <GitHubIcon fontSize="inherit" />
+                      &nbsp;user/repository
+                    </Typography>
+                  </Grid>
+
+                  <Grid item>
+                    <Typography variant="h6">#revision</Typography>
+                  </Grid>
+
+                  <Grid item>
+                    <Typography variant="h6">YYYY/MM/DD</Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+
+              <Box mb={4}>
+                <Chart
+                  data={[
+                    { type: "Unduplicated lines", count: 90 },
+                    { type: "Duplicated lines", count: 10 },
+                  ]}
+                >
+                  <Animation />
+                  <EventTracker />
+                  <Legend />
+                  <Palette
+                    scheme={[
+                      theme.palette.primary.main,
+                      theme.palette.secondary.main,
+                    ]}
+                  />
+                  <Tooltip />
+
+                  <PieSeries valueField="count" argumentField="type" />
+                </Chart>
+              </Box>
+
+              <Box mb={4}>
+                <Typography gutterBottom variant="h5">
+                  Statistics
                 </Typography>
-              </Grid>
 
-              <Grid item>
-                <Typography variant="h6">#revision</Typography>
-              </Grid>
+                <TableContainer component={Paper}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Format</TableCell>
+                        <TableCell align="right">Files analyzed</TableCell>
+                        <TableCell align="right">Clones found</TableCell>
+                        <TableCell align="right">Duplicated lines</TableCell>
+                      </TableRow>
+                    </TableHead>
 
-              <Grid item>
-                <Typography variant="h6">YYYY/MM/DD</Typography>
-              </Grid>
-            </Grid>
-          </Box>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell component="th" scope="row">
+                          YAML
+                        </TableCell>
+                        <TableCell align="right">30</TableCell>
+                        <TableCell align="right">112</TableCell>
+                        <TableCell align="right">1212 (12 %)</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Box>
 
-          <Box mb={4}>
-            <Chart data={data}>
-              <Animation />
-              <EventTracker />
-              <Legend />
-              <Palette
-                scheme={[
-                  theme.palette.primary.main,
-                  theme.palette.secondary.main,
-                ]}
-              />
-              <Tooltip />
-
-              <PieSeries valueField="count" argumentField="type" />
-            </Chart>
-          </Box>
-
-          <Box mb={4}>
-            <Typography gutterBottom variant="h5">
-              Statistics
-            </Typography>
-
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Format</TableCell>
-                    <TableCell align="right">Files analyzed</TableCell>
-                    <TableCell align="right">Clones found</TableCell>
-                    <TableCell align="right">Duplicated lines</TableCell>
-                  </TableRow>
-                </TableHead>
-
-                <TableBody>
-                  <TableRow>
-                    <TableCell component="th" scope="row">
-                      YAML
-                    </TableCell>
-                    <TableCell align="right">30</TableCell>
-                    <TableCell align="right">112</TableCell>
-                    <TableCell align="right">1212 (12 %)</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Box>
-
-          <Typography gutterBottom variant="h5">
-            Duplicates
-          </Typography>
-
-          <Box mb={4}>
-            <Paper>
-              <Typography variant="body1">
-                <SyntaxHighlighter language="javascript" style={vs2015}>
-                  const a = 0;&lt;
-                </SyntaxHighlighter>
+              <Typography gutterBottom variant="h5">
+                Duplicates
               </Typography>
-            </Paper>
-          </Box>
+
+              <Box mb={4}>
+                <Paper>
+                  <Typography variant="body1">
+                    <SyntaxHighlighter language="javascript" style={vs2015}>
+                      const a = 0;&lt;
+                    </SyntaxHighlighter>
+                  </Typography>
+                </Paper>
+              </Box>
+            </>
+          )}
         </Container>
       </main>
-
-      <footer>
-        <Container>
-          <Typography align="center" gutterBottom variant="body2">
-            Powered by jscpd-service.
-          </Typography>
-        </Container>
-      </footer>
     </>
   );
 };
--- packages/viewer/webpack.config.js
+++ packages/viewer/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
   },
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: "resources", to: "dist" }],
+      patterns: [{ from: "resources" }],
     }),
   ],
   resolve: {
